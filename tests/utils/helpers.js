072e7e86b15f47f4eb3fda17afb7bca68981aaaa
daniel.igor.rodrigues <daniel.igor.rodrigues@gmail.com>
2024-08-27 02:56:33 -0300

    exclusão do arquivo env que não estava sendo utilizado, e ajuste de sintaxes

--- tests/utils/helpers.js
+++ tests/utils/helpers.js
@@ -11,56 +11,55 @@ export function generateRandomUser(isAdmin = false) {
         password: faker.internet.password(),
         administrador: isAdmin ? 'true' : 'false'
     };
-}
+};
 
 async function createUser(request, data) {
-    const newUser = await request.post(`${base_api}/usuarios/`, { data })
-    expect(newUser.ok()).toBeTruthy()
+    const newUser = await request.post(`${base_api}/usuarios/`, { data });
+    expect(newUser.ok()).toBeTruthy();
 };
 
 async function deleteUser(request, idUser) {
-    const deleteUser = await request.delete(`${base_api}/usuarios/${idUser}`)
-    expect(deleteUser.ok()).toBeTruthy()
+    const deleteUser = await request.delete(`${base_api}/usuarios/${idUser}`);
+    expect(deleteUser.ok()).toBeTruthy();
 };
 
 async function queryUserExist(request, email) {
-    const response = await request.get(`${base_api}/usuarios`)
-    let users = await response.json()
-    users = users.usuarios
+    const response = await request.get(`${base_api}/usuarios`);
+    let users = await response.json();
+    users = users.usuarios;
     return users.find(u => u.email === email);
-}
+};
 
 export async function ensuredUser(request, data) {
-    let user = await queryUserExist(request, data.email)
+    let user = await queryUserExist(request, data.email);
     while (user) {
         await deleteUser(request, user._id);
         user = await queryUserExist(request, data.email);
-    }
-    await createUser(request, data)
+    };
+    await createUser(request, data);
 };
 
 export async function deleteNewUser(request, email) {
-    const user = await queryUserExist(request, email)
+    const user = await queryUserExist(request, email);
 
     if (user) {
         await deleteUser(request, user._id)
-    }
+    };
 };
 
 export async function getToken(request) {
-    tokenUser = generateRandomUser(true)
-    await ensuredUser(request, tokenUser)
+    tokenUser = generateRandomUser(true);
+    await ensuredUser(request, tokenUser);
     const user = {
         "email": tokenUser.email,
         "password": tokenUser.password
-    }
-    const response = await request.post(`${base_api}/login/`, { data: user })
-    expect(response.ok()).toBeTruthy()
-    let token = await response.json()
-    token = token.authorization
-    return token
+    };
+    const response = await request.post(`${base_api}/login/`, { data: user });
+    expect(response.ok()).toBeTruthy();
+    let token = await response.json();
+    token = token.authorization;
+    return token;
 };
-
 
 export async function ensuredProduct(request, product) {
     const token = await getToken(request);
@@ -79,7 +78,7 @@ export async function ensuredProduct(request, product) {
                 'Content-Type': 'application/json'
             }
         });
-    }
+    };
 
     const response = await request.post(`${base_api}/produtos`, {
         data: product,
@@ -92,5 +91,3 @@ export async function ensuredProduct(request, product) {
 
     await deleteNewUser(request, tokenUser.email);
 }
-
-
